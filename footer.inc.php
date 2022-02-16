ab4a75be16ba8a2ea071230383e92b09f359ab02
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro.local>
2022-02-16 23:19:22 +0100

    style: added the html and css as a template

---
+++ footer.inc.php
@@ -0,0 +1,1 @@
+<footer>hallo dit is de footer</footer>
--- index.php
+++ index.php
@@ -19,10 +19,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Swarm App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
 </head>
 <body>
+<?php include_once("header.inc.php")?>
     <?php // todo3 : lus over je checkins en print deze visueel af zoals op de screenshots/screenshot1.png?>
-
+<section class="content">
+    <div class="post">
+        <img class="post__userImage" src="images/image-1.png" alt="profile picture">
+        <div class="post__content">
+        <span class="post__content--username">Jesse</span>
+        <span class="post__content--place">Assembly3.0</span>
+        <span class="post__content--location">San Francisco, CA</span>
+        <p class="post__content--comment">LE WORK.</p>
+        </div>
+    </div>
+    <div class="post">
+        <img class="post__userImage" src="images/image-1.png" alt="profile picture">
+        <div class="post__content">
+            <span class="post__content--username">Jesse</span>
+            <span class="post__content--place">Assembly3.0</span>
+            <span class="post__content--location">San Francisco, CA</span>
+            <p class="post__content--comment">LE WORK.</p>
+        </div>
+    </div>
+</section>
     <?php // todo4 : zorg dat je header en footer opgehaald wordt vanuit footer.inc.php en header.inc.php zodat je deze kan hergebruiken op meerdere schermen?>
+    <?php include_once("footer.inc.php")?>
 </body>
 </html>
